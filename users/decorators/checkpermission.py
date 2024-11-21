93eb0deba567d119642a73cba6770fcd2b55a419
Ali Noori <matineali@gmail.com>
2024-11-21 19:20:21 +0330

    correct auth

--- users/decorators/checkpermission.py
+++ users/decorators/checkpermission.py
@@ -1,25 +1,28 @@
 from functools import wraps
 from rest_framework.views import APIView
-from django.http import JsonResponse
+from rest_framework.exceptions import PermissionDenied, NotAuthenticated
 
 def hasPermission(permission):
     def decorator(view_func):
         # بررسی اینکه دکوراتور فقط برای کلاس‌های APIView استفاده می‌شود
         if not isinstance(view_func, type) or not issubclass(view_func, APIView):
-            raise TypeError("The @has_permission decorator can only be applied to APIView classes.")
+            raise TypeError("The @hasPermission decorator can only be applied to APIView classes.")
 
         # تزئین متد dispatch برای مدیریت درخواست‌ها
         original_dispatch = view_func.dispatch
 
         @wraps(view_func)
         def dispatch_wrapper(self, request, *args, **kwargs):
-            if request.user.is_authenticated:
-                if request.user.has_perm(permission):
-                    return original_dispatch(self, request, *args, **kwargs)
-                else:
-                    return JsonResponse({'detail': 'Access denied'}, status=403)
-            else:
-                return JsonResponse({'detail': 'Authentication required'}, status=401)
+            # بررسی احراز هویت
+            if not request.user or not request.user.is_authenticated:
+                raise NotAuthenticated(detail="Authentication required")
+
+            # بررسی دسترسی
+            if not request.user.has_perm(permission):
+                raise PermissionDenied(detail="Access denied")
+
+            # ادامه اجرای view اصلی
+            return original_dispatch(self, request, *args, **kwargs)
 
         view_func.dispatch = dispatch_wrapper
         return view_func
